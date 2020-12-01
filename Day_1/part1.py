278c679be23e4b28deb0213d62fc86e7e71dd1f6
Watrick117 <60557071+Watrick117@users.noreply.github.com>
2020-12-01 16:52:09 -0500

    Update part1.py

--- Day_1/part1.py
+++ Day_1/part1.py
@@ -4,7 +4,31 @@ Day 1 part 1 of Advent of Code 2020
 """
 
 import csv
-with open('expense_report.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row)
+import sys
+
+with open("expense_report.csv","r") as csvFile:
+  reader = csv.reader(csvFile)
+  data = []
+  for row in reader:
+     if len(row) !=0:
+        data = data + row
+csvFile.close()
+
+print(f'{len(data) = }')
+print()
+print(data)
+print()
+
+for i in range(1, len(data)+1):
+    for j in range(1, len(data)+1):
+
+        if int(data[i-1]) == int(data[j-1]):
+            break
+            
+        if int(data[i-1]) + int(data[j-1]) == 2020:
+            data_i_plus_data_j = int(data[i-1]) * int(data[j-1])
+            print('The two expenses that add up to 2020. Are %s and %s.' % (data[i-1],data[j-1]))
+            print('And there multiplicant is %s' % (int(data[i-1]) * int(data[j-1])))
+            print()
+
+sys.exit()

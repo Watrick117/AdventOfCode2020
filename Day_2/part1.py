17573a6ca8d4a6254d3f0df17b6cbec15c05294d
Watrick117 <60557071+Watrick117@users.noreply.github.com>
2020-12-02 22:22:58 -0500

    Update part1.py

--- Day_2/part1.py
+++ Day_2/part1.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python3
 """Patrick Woltman
-Day 2 part 1 of Advent of Code 2020
+--- Day 2: Password Philosophy ---
+
+Your flight departs in a few days from the coastal airport; the easiest way down to the coast from here is via toboggan.
+
+The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day. "Something's wrong with our computers; we can't log in!" You ask if you can take a look.
+
+Their password database seems to be a little corrupted: some of the passwords wouldn't have been allowed by the Official Toboggan Corporate Policy that was in effect when they were chosen.
+
+To try to debug the problem, they have created a list (your puzzle input) of passwords (according to the corrupted database) and the corporate policy when that password was set.
+
+For example, suppose you have the following list:
+
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+
+Each line gives the password policy and then the password. The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid. For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
+
+In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
+
+How many passwords are valid according to their policies?
+
 """
 
 import csv
@@ -17,17 +38,12 @@ csvFile.close()
 validPasswords = 0
 invalidPasswords = 0
 
-print(f'{len(passwords) = }')
-print()
-
 for i in range(0, len(passwords)):
     passwords[i] = passwords[i].split(" ")
     passwords[i][1] = passwords[i][1].replace(':', '')
 
     
 for j in range(0, len(passwords)):
-    print()
-    print(f'{j = }')
 
     temp = passwords[j][0].split("-")
 
@@ -40,17 +56,8 @@ for j in range(0, len(passwords)):
 
     if  int(temp[0]) <= count <= int(temp[1]):
         validPasswords += 1
-        print("VALID")
     elif count < int(temp[0]) or count > int(temp[1]):
         invalidPasswords += 1
-        print("INVALID")
-
-    print(f'{passwords[j][2] = }')
-
-    print(count >= int(temp[0]) <= int(temp[1]))
-    print(f'{temp = }')
-    print(f'{passwords[j][1] = }')
-    print(f'{count = }')
 
     if (count >= int(temp[0]) <= int(temp[1])) == True and (count < int(temp[0]) < int(temp[1])) == True:
         input('Whats wrong with this section.\n')
@@ -59,8 +66,7 @@ print()
 print(f'{invalidPasswords = }')
 print(f'{validPasswords = }')
 
-programWorked = len(passwords) - invalidPasswords - validPasswords
-if programWorked == 0:
+if len(passwords) - invalidPasswords - validPasswords == 0:
     print("Program Worked")
 
 sys.exit()

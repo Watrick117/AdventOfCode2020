18b5a6969c5b95e4fc09b9ffdd0b795a394a903c
Watrick117 <60557071+Watrick117@users.noreply.github.com>
2020-12-05 22:42:50 -0500

    Day_4 Upadate

--- Day_4/part2.py
+++ Day_4/part2.py
@@ -74,10 +74,13 @@ Count the number of valid passports - those that have all required fields and va
 
 import sys
 
-file1 = open('passports.bat', 'r') 
+file1 = open('passports_test.bat', 'r') 
 unsorted = file1.readlines() 
 
 passports = []
+dictionary_passports = {}
+sorted_passports = []
+
 required_fields = [
     'byr',
     'iyr',
@@ -95,19 +98,63 @@ temp = ''
 
 for i in range(0, len(unsorted)):
     if unsorted[i] != '\n':
-        temp = str(temp + ' ' + unsorted[i]).replace(':', ' ').strip('\n')
+        #temp = str(temp + ' ' + unsorted[i]).replace(':', ' ').strip('\n')
+        temp = str(temp + ' ' + unsorted[i]).strip('\n')
     elif unsorted[i] == '\n':
         passports.append(temp.strip().split())
         temp = ''
 
 for i in range(0, len(passports)):
+    for j in range(0, len(passports[i])):
+        tempB = passports[i][j].replace(':', ' ').split(' ')
+        dictionary_passports[tempB[0]] = tempB[1]
+    sorted_passports.append(dictionary_passports)
+    print(dictionary_passports)
+    dictionary_passports.clear()
+
+"""for i in range(0, len(passports)):
     count = 0
     for j in range(0, len(passports[i])):
         if passports[i][j][0:3] in required_fields:
             count += 1
             if count == len(required_fields):
-                valid_passports += 1
+                valid_passports += 1"""
 
-print(f'{valid_passports = }')
+for i in range(0, len(sorted_passports)):
+    for j in range(0, len(sorted_passports[i])):
+        try:
+            if sorted_passports[1] in required_fields:
+                count += 1
+                print("its in here")
+        except:
+            print('')
+
+"""
+byr (Birth Year) - four digits; at least 1920 and at most 2002.
+iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+hgt (Height) - a number followed by either cm or in:
+    If cm, the number must be at least 150 and at most 193.
+    If in, the number must be at least 59 and at most 76.
+hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+pid (Passport ID) - a nine-digit number, including leading zeroes.
+cid (Country ID) - ignored, missing or not.
+"""
+
+for i in range(0, len(sorted_passports)):
+    print(f'{sorted_passports[i] = }')
+    
+print('---------------------------------------------------')
+print(f'{dictionary_passports = }')
+print('---------------------------------------------------')
+print(f'{sorted_passports = }')
+print('---------------------------------------------------')
+print(f'{len(sorted_passports) = }') #259
+
+try:
+  print(x)
+except:
+  print("An exception occurred") 
 
 sys.exit()

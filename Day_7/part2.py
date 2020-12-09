c5fcf55333c4aee61b6851c1159ffd873588fa8e
Watrick117 <60557071+Watrick117@users.noreply.github.com>
2020-12-08 20:23:57 -0500

    Day 7 Setup

---
+++ Day_7/part2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Patrick Woltman
+
+"""
+
+# file input
+file1 = open('regulations_sample.txt', 'r') 
+unsorted = file1.read().splitlines()
+file1.close()
+
+import sys
+
+sys.exit()
